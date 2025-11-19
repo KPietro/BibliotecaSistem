e8ad1ad650c3a149d2d05599438337af9691dc38
Pietro Soares Rios <pietro.rios2008@gmail.com>
2025-11-19 10:56:52 -0300

    v4
    Correção e modificação grande base codigo de um colega pois minha versão estava tendo problemas com dependencias codigo ainda será melhorado

---
+++ BibliotecaSistem/Models/Livro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaSistem.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string NumeroSerial { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public int Paginas { get; set; } = 0;
+        public bool Disponivel { get; set; } = true;
+    }
+}
